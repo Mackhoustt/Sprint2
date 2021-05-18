aa55b6fdaf061f4865eec59b67e3b6ab6ab9b768
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-05-18 18:43:34 -0500

    Se agrego un addeventlisner cuando pasen el cursos sobre boton de pagar, un boton para registrar un usuario y que sus datos se alamacenen en localstorage

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
-
+//algotimo para hacer que el formulario tenga 3 requisitos necesarios
 const nombre= document.getElementById("nombre");
 const email= document.getElementById("email");
 const edad= document.getElementById("edad");
 const form= document.getElementById("form");
-const local= document.getElementById();
 let entrar =true;
+localStorage.setItem("nombre",JSON.stringify(nombre));
+
 function aceptar(entrar){
   
 if (nombre.value.length >0 &&  email.value.length >0 && edad.value.length>0){
@@ -20,3 +21,21 @@ while (entrar==true){
    }
 }
 
+//un advenlistener que cumple la funcion: cuando el mouse salga del boton cambie su color
+const local= document.getElementById("local");
+local.addEventListener('mouseout',function(){
+    local.style.background = 'cyan';
+});
+
+
+
+//localstorage que registra el nombre, correo y edad
+function GuardarLocalStorage(){
+    let persona = {
+        nombre: nombre.value,
+        email: email.value,
+        edad: edad.value
+    }
+    localStorage.setItem("Persona",JSON.stringify(persona));
+    alert("Se registro satisfactoriamente. *Por favor termina el formulario*")
+}
